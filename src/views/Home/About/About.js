b7812a30534641bc6bb5205b9ae67dbe3537fde5
Ray Olivier <rayolivier@outlook.com>
2018-10-23 09:06:57 -0500

    put basic info in about page

--- src/views/Home/About/About.js
+++ src/views/Home/About/About.js
@@ -1,11 +1,36 @@
 import React from "react"
-import MapContainer from "../../../components/MapContainer/MapContainer"
+// import MapContainer from "../../../components/MapContainer/MapContainer"
 
 export default function About() {
   return (
-    <div className="about">
-      <div>About</div>
-      <MapContainer />
+    <div className="about-page">
+      <div>About HealthQueery</div>
+      <p>
+        This website was made to list LGBTQ+ healthcare providers and allow the
+        community to review them. The goal is to make it easier to find
+        inclusive healthcare for everyone. HealthQueery is also a personal
+        project and was built by a single developer.
+      </p>
+      {/* <MapContainer /> */}
+      <div className="contact-container">
+        <h1>Contact</h1>
+        <div>Email: healthqueery@outlook.com</div>
+      </div>
+      <div className="faq-section">
+        <h1>FAQ</h1>
+        <div className="q-and-a">
+          <h2 className="question">
+            Is HealthQueery only for transgender people?
+          </h2>
+          <p className="answer">
+            No. HealthQueery is trans-inclusive and there are a lot of listings
+            that pertain to medical transition, but there are also listings for
+            everything from HIV screening to LGBTQ+ friendly primary care
+            physicians. The goal is to help everyone in the LGBTQ+ community
+            find quality healthcare.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
